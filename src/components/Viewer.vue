0c7434d50f847c6ca3931c0caa6fb3e625f0f71e
ouzian <judgeou@vip.qq.com>
2023-04-26 11:29:58 +0800

    random pick

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import dayjs from 'dayjs'
 
 interface IPost {
   id: number,
@@ -17,13 +18,37 @@ const addition_tags = [ 'rating:General' ]
 const tag_input = ref('')
 const posts = ref([] as IPost[])
 const isLoading = ref(false)
+const isRandom = ref(true)
+const random_startdate = ref('2015-01-01')
+const random_enddate = ref(dayjs().format('YYYY-MM-DD'))
 
 async function search () {
   isLoading.value = true
-  const tags = `${tag_input.value} ${addition_tags.join(' ')}`
+  const tags = `${tag_input.value} ${addition_tags.join(' ')} ${get_random_tags()}`
   const res = await fetch(`https://danbooru.donmai.us/posts.json?limit=20&tags=${tags}`)
   posts.value = await res.json()
   isLoading.value = false
+}
+
+function get_random_tags () {
+  if (isRandom.value) {
+    // 定义开始和结束日期
+    const startDate = dayjs(random_startdate.value)
+    const endDate = dayjs(random_enddate.value)
+
+    // 计算开始和结束日期之间的天数
+    const days = endDate.diff(startDate, 'day');
+
+    // 随机选择一个天数
+    const randomDays = Math.floor(Math.random() * days);
+
+    // 创建新日期并添加随机天数
+    const resultDate = startDate.add(randomDays, 'day');
+
+    return `date:${resultDate.format('YYYY-MM-DD')} order:random`
+  } else {
+    return ''
+  }
 }
 
 async function copy_img_tags (post: IPost) {
@@ -41,7 +66,15 @@ async function copy_img_tags (post: IPost) {
 
 <template>
   <div>
-    <input type="text" v-model="tag_input" style="width: 300px;">
+    <input type="text" placeholder="tags" v-model="tag_input" style="width: 300px;">
+    <label>
+      <input type="checkbox" v-model="isRandom"> random
+    </label>
+    <span v-if="isRandom">
+      <input type="date" v-model="random_startdate" />
+      <input type="date" v-model="random_enddate" />
+    </span>
+
   </div>
 
   <div style="margin-top: 8px;">
