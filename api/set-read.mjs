3947adde0d26e0b31a5951f2bfa6cf8b8778ff9c
ouzian <judgeou@vip.qq.com>
2023-07-21 17:29:53 +0800

    api

--- api/set-read.mjs
+++ api/set-read.mjs
@@ -4,8 +4,8 @@ import { config } from 'dotenv'
 config()
 
 export default async function handler (req, res) {
-  const { name } = req.query
-  const r = await kv.get(name + '_hasRead');
+  const { name, value } = req.query
+  const r = await kv.set(name + '_hasRead', value);
 
   res.status(200).json(r)
 }
